0c4e30c6c647e933849e262bf6152f63516986c0
dgop92 <inevaup@gmail.com>
2022-06-13 11:02:59 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Portfolio
+
+Repository of my portfolio, there is nothing more to say
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "normalize.css";
+import React from "react";
+
+export default function App() {
+  return (
+    <div>hellow word</div>
+  );
+}
